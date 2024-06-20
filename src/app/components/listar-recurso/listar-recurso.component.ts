54c4d9c8e048e4bf7133c59de39b4ab0fd631185
Cruz Cesar <106643035+C3sarCruz@users.noreply.github.com>
2024-06-20 11:24:39 -0600

    Rutinas3.0
    update rutinas

--- src/app/components/listar-recurso/listar-recurso.component.ts
+++ src/app/components/listar-recurso/listar-recurso.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RecursoService } from './../../services/recurso.service';
+import { ToastrService } from 'ngx-toastr';
 import { Recurso } from 'src/app/models/recurso';
+import { RutinasService } from 'src/app/services/rutinas.service';
+import { enviroment } from 'src/enviroments/enviroment';
+import { Observable } from 'rxjs';
+import { DataUser, LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'rutinas',
@@ -8,52 +13,72 @@ import { Recurso } from 'src/app/models/recurso';
   styleUrls: ['./listar-recurso.component.css']
 })
 export class ListarRecursoComponent implements OnInit{
-    mostrarSinFallos: boolean = false;
-  mostrarConFallos: boolean = false;
-  listRutina = false;
+  listRutina = false ;
   rutinas = true;
   mapaSitio = false;
-    mostrarDiv(opcion: string) {
-        if (opcion === 'SinFallos') {
-            this.mostrarSinFallos = true;
-            this.mostrarConFallos = false;
-        } else if (opcion === 'ConFallos') {
-            this.mostrarSinFallos = false;
-            this.mostrarConFallos = true;
-        }
-    }
-
-
-
-  listRecursos: Recurso[] = [];
-
-  filterPost = '';
-
-  filter: string = '';
-
-  constructor(private _recursoService: RecursoService) {}
-
+  listRutinas : any [] = [];
+  mostrarRutina: any;
+  nombrePerfil: Observable<DataUser>;
+  constructor(private loginService: LoginService, private _rutinasService: RutinasService, private toastr: ToastrService) {
+    this.caragaRutinas();
+    this.nombrePerfil = loginService.getDataUser();
+  }
+  caragaRutinas(): void {
+    this._rutinasService.getRutina().then(
+      (ejercicios) => {
+        this.listRutinas = ejercicios;
+        setTimeout(() => {
+          this.listRutinas.forEach((rutina: any) => {
+            setTimeout(() => {
+              if(rutina.imagen) {
+                const srcImage: any = document.getElementById('image' + rutina._id);
+                srcImage.src = `${enviroment.urlBackEnd}/${rutina.imagen}`;
+              } else {
+                const srcImage: any = document.getElementById('image' + rutina._id);
+                srcImage.src = `${enviroment.urlBackEnd}/default.png`;
+              }
+            }, 250)
+          });
+        }, 500)
+      }
+    )
+  }
+  mostrar(rutina: any) {
+    this.listRutina = true;
+    this.rutinas = false;
+    this.mostrarRutina = rutina;
+    setTimeout(() => {
+      if(rutina.imagen) {
+        const srcImage: any = document.getElementById('imagen' + rutina._id);
+        srcImage.src = `${enviroment.urlBackEnd}/${rutina.imagen}`;
+      } else {
+        const srcImage: any = document.getElementById('imagen' + rutina._id);
+        srcImage.src = `${enviroment.urlBackEnd}/default.png`;
+      }
+    }, 250)
+    console.info('mostrarRutina:', this.mostrarRutina)
+  }
   ngOnInit(): void {
-    this.obtenerRecursos();
-    this.mostrarDiv('SinFallos');
+
   }
 
-  obtenerRecursos() {
+/*   obtenerRecursos() {
     this._recursoService.getRecursos().subscribe(data => {
       console.log(data);
       this.listRecursos = data;
     }, error => {
       console.log(error);
     })
-  }
+  } */
 
-  eliminarRecurso(id:any) {
+/*   eliminarRecurso(id:any) {
     this._recursoService.eliminarRecurso(id).subscribe(data => {
+      this.toastr.error('El recurso fue eliminado con exito');
       this.obtenerRecursos();
     }, error => {
       console.log(error);
     } )
-  }
+  } */
 
   muestra() {
     this.listRutina = true;
@@ -62,7 +87,7 @@ export class ListarRecursoComponent implements OnInit{
     muestraMapa() {
     this.mapaSitio = true;
     }
-
+  
   cerrarMapa() {
     this.mapaSitio = false;
   }
